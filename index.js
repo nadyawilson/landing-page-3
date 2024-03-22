64c4a347fd6f9d6883ca8e9986d694073ed7ab85
nadyawilson <164531702+nadyawilson@users.noreply.github.com>
2024-03-22 16:42:49 +0100

    Add files via upload
    Landing page

---
+++ index.js
@@ -0,0 +1,98 @@
+gsap.from(".nav-logo,.nav-links > a", 2,{
+    top:"30px",
+    opacity:0,
+    ease:"power4.inOut",
+    delay:1,
+    stagger:{
+        amount:0.3,
+    }
+} 
+
+
+
+
+)
+gsap.from("h1", 2,{
+    y:"50",
+    opacity:0,
+    ease:"power4.inOut",
+    delay:1.5,
+    stagger:{
+        amount:0.3,
+    }
+} 
+
+
+
+
+)
+gsap.from(".play-wrapper, .pattern, .copy", 2,{
+    scaleY:"0",
+    ease:"power3.inOut",
+    delay:2.5,
+    stagger:{
+        amount:0.3,
+    }
+} 
+
+
+
+
+)
+gsap.from(".hr", 2,{
+    width:"0",
+    ease:"power3.inOut",
+    delay:3,
+   
+} 
+
+
+
+
+)
+gsap.from(".btns", 2,{
+    x:"50",
+    opacity:0,
+    ease:"power3.inOut",
+    delay:3,
+   
+} 
+
+
+
+
+)
+gsap.from(".play-btn", 2,{
+    scale:"0",
+    ease:"power3.inOut",
+    delay:3,
+   
+} 
+
+
+
+
+)
+gsap.from(".hero-wrapper", 2,{
+    width:"100%",
+    ease:"power3.inOut",
+    delay:3,
+   
+} 
+
+
+
+
+)
+gsap.from(".arrow", 2,{
+    scale:"0",
+    ease:"power3.inOut",
+    delay:3,
+   
+} )
+gsap.from(".marque", 2,{
+    bottom:"-10rem",
+    ease:"power3.inOut",
+    delay:4,
+   
+} )
